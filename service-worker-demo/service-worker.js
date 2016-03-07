67c07ec8eca277acdbfc04124361c5b870136da6
YDSS <y.dsheng10@gmail.com>
2016-03-07 21:57:00 +0800

    debug hijack

--- service-worker-demo/service-worker.js
+++ service-worker-demo/service-worker.js
@@ -7,9 +7,11 @@ self.addEventListener('activate', ev => {
 self.addEventListener('fetch', ev => {
     var reqUrl = ev.request.url;
     console.log('hijack request: ' + reqUrl);
+    console.log(ev.request);
     
     // 若是text.css的请求被拦截，返回伪造信息
     if (reqUrl.indexOf('text.css') > -1) {
+        console.log('hijack text.css');
         ev.respondWith(
             new Response('hahah', {
                 headers: {'Content-Type': 'text/css'}
